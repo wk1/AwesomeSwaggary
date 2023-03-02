e36379c91330852cc103ad25d6e77d9b8c1a771a
Christian Hoock <9932295+wk1@users.noreply.github.com>
2023-03-02 16:46:13 +0100

    Add get frame viewmodifier to capsule the whole background geo reader -> perferences key flow to read a views frame in swiftui (#1)

--- Sources/AwesomeSwaggary/AwesomeSwaggary.swift
+++ Sources/AwesomeSwaggary/AwesomeSwaggary.swift
@@ -1,6 +1,6 @@
 public struct AwesomeSwaggary {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
+  public private(set) var text = "Hello, World!"
+  
+  public init() {
+  }
 }
