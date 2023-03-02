5b1dea5f8c49c618ddf2271b30be22641d76a95c
Christian Hoock <9932295+wk1@users.noreply.github.com>
2023-03-02 16:29:43 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AwesomeSwaggary
+
+A description of this package.
---
+++ Sources/AwesomeSwaggary/AwesomeSwaggary.swift
@@ -0,0 +1,6 @@
+public struct AwesomeSwaggary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
