fcdd629f9f65e83cf946830831c8965f07ff36ed
Christian Hoock <9932295+wk1@users.noreply.github.com>
2023-03-02 19:38:59 +0100

    Add totally swag scroll view

---
+++ Sources/AwesomeSwaggary/View/SwaggerScrollView.swift
@@ -0,0 +1,50 @@
+//
+//  StupidSexyScrollView.swift
+//  
+//
+//  Created by Christian Hoock on 02.03.23.
+//
+
+import SwiftUI
+
+@available(iOS 16.0, *)
+struct SwaggerScrollView<Content: View>: View {
+  
+  var axes: Axis.Set = [.vertical]
+  var showsIndicators = true
+  
+  @State private var currentFrame: CGRect = .zero
+  @State private var currentContentFrame: CGRect = .zero
+  @ViewBuilder var content: () -> Content
+  
+  var body: some View {
+    ScrollView(axes, showsIndicators: showsIndicators) {
+      content()
+        .frameChanged { rect in
+          currentContentFrame = rect
+        }
+    }
+    .padding(.top, 1) // this will prevent the scrollview to extend under the safe area
+    .frame(minHeight: 0, maxHeight: currentContentFrame.height)
+    .scrollDisabled(currentContentFrame.height <= currentFrame.height )
+    .frameChanged { rect in
+      currentFrame = rect
+    }
+  }
+}
+
+@available(iOS 16.0, *)
+struct SwaggerScrollView_Previews: PreviewProvider {
+  static var previews: some View {
+    SwaggerScrollView {
+      Text("Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla")
+    }
+    
+    VStack(spacing: 0) {
+      SwaggerScrollView {
+        Text("Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla")
+      }
+      Spacer(minLength: 700)
+    }
+  }
+}
