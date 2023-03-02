0cc998ef893002e4022bea4e3b2b2b0e56e4761e
Christian Hoock <9932295+wk1@users.noreply.github.com>
2023-03-02 21:36:00 +0100

    Update content view

--- Sources/AwesomeSwaggary/View/SwaggerScrollView/SwaggerScrollContentView.swift
+++ Sources/AwesomeSwaggary/View/SwaggerScrollView/SwaggerScrollContentView.swift
@@ -8,19 +8,26 @@
 import SwiftUI
 
 @available(iOS 16.0, *)
-struct SwaggerScrollContentView: View {
-    
-    var coordinateSpace: CoordinateSpace
-    var frameChangeCallback: ((_ frame: CGRect) -> Void)? = nil
-    
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct SwaggerScrollContentView<Content: View>: View {
+  
+  var coordinateSpace: CoordinateSpace
+  var frameChangeCallback: ((_ frame: CGRect) -> Void)? = nil
+  
+  @ViewBuilder var content: () -> Content
+  
+  var body: some View {
+    content()
+      .frameChanged { rect in
+        frameChangeCallback?(rect)
+      }
+  }
 }
 
 @available(iOS 16.0, *)
 struct SwaggerScrollContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwaggerScrollContentView(coordinateSpace: .named("SwagSpace"))
+  static var previews: some View {
+    SwaggerScrollContentView(coordinateSpace: .named("SwagSpace")) {
+      VeganIpsumView()
     }
+  }
 }
