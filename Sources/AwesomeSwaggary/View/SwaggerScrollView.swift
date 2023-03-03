5d023359ab0bf7bf158a5aa8d3712c925a9b7da8
Christian Hoock <9932295+wk1@users.noreply.github.com>
2023-03-03 15:30:39 +0100

    Add fadeout/in functionality

--- Sources/AwesomeSwaggary/View/SwaggerScrollView.swift
+++ Sources/AwesomeSwaggary/View/SwaggerScrollView.swift
@@ -36,6 +36,39 @@ public struct SwaggerScrollView<Content: View>: View {
   
   private let coordinateSpaceName = UUID()
   
+  let length = 100.0
+  let edges: Edge.Set = .vertical
+  
+  let opacityTolerance: CGFloat = 10.0
+  
+  var topOpacity: CGFloat {
+    let value = min(1, max(0, 1 - (offset.y/opacityTolerance)))
+    print("topOpacity: \(value)")
+    return value
+  }
+  
+  var bottomOpacity: CGFloat {
+    let justifiedVerticalOffset = offset.y - (maxOffset.y - opacityTolerance)
+    let fixedVerticalOffset: CGFloat = min(opacityTolerance, max(0, justifiedVerticalOffset))
+    
+    let value = min(1, max(0, (fixedVerticalOffset/opacityTolerance)))
+    print("bottomOpacity: \(value)")
+    return value
+  }
+  
+  var gradient: some View {
+    LinearGradient(
+      gradient: Gradient(
+        stops: [
+          .init(color: .clear, location: 0),
+          .init(color: .black, location: 1.0)
+        ]
+      ),
+      startPoint: .bottom, endPoint: .top
+    )
+    .frame(height: length)
+  }
+  
   public var body: some View {
     ScrollView(axes, showsIndicators: showsIndicators) {
       content()
@@ -51,6 +84,29 @@ public struct SwaggerScrollView<Content: View>: View {
     .scrollDisabled(round(currentContentFrame.height) <= round(currentFrame.height))
     .frameChanged(coordinateSpace: .named(coordinateSpaceName)) { rect in
       currentFrame = rect
+    }
+    .mask {
+      VStack(spacing: 0) {
+        if edges.contains(.top) {
+          ZStack {
+            gradient
+              .scaleEffect(y: -1)
+            Color.black
+              .frame(height: length)
+              .opacity(topOpacity)
+          }
+        }
+        Color.black
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+        if edges.contains(.bottom) {
+          ZStack {
+            gradient
+            Color.black
+              .frame(height: length)
+              .opacity(bottomOpacity)
+          }
+        }
+      }
     }
   }
 }
