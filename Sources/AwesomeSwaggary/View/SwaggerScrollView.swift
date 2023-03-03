a70bef3946e9c3a8554c6b5392a648870fa7381d
Christian Hoock <9932295+wk1@users.noreply.github.com>
2023-03-03 10:54:15 +0100

    Add content offset to swagger scroll (#3)
    * Add folder and swag scroll content class
    
    * Add vegan ipsum view
    
    * Add offset logic
    
    * Update content view
    
    * Add public access modifiers. Also add coordinateSpace to SwaggerScrollView
    
    * Deactivate extra padding since it breaks the offset observation
    
    * Replace padding top/bottom 1 with clipped modifier to prevent scrollview from extending under safe area insets
    
    * Add a little hint to markdown support
    
    * Remove unnecessary content view
    
    * Update coordinate space name

--- Sources/AwesomeSwaggary/View/SwaggerScrollView.swift
+++ Sources/AwesomeSwaggary/View/SwaggerScrollView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @available(iOS 16.0, *)
-struct SwaggerScrollView<Content: View>: View {
+public struct SwaggerScrollView<Content: View>: View {
   
   var axes: Axis.Set = [.vertical]
   var showsIndicators = true
@@ -17,19 +17,50 @@ struct SwaggerScrollView<Content: View>: View {
   @State private var currentContentFrame: CGRect = .zero
   @ViewBuilder var content: () -> Content
   
-  var body: some View {
+  var scrollAction: ((_ offset: CGPoint, _ maxOffset: CGPoint) -> Void)?
+  
+  public init(axes: Axis.Set = [.vertical], showsIndicators: Bool = true,  content: @escaping () -> Content) {
+    self.content = content
+  }
+  
+  var offset: CGPoint {
+    .init(x: -currentContentFrame.origin.x, y: -currentContentFrame.origin.y)
+  }
+  
+  var maxOffset: CGPoint {
+    .init(
+      x: currentContentFrame.width - currentFrame.width,
+      y: currentContentFrame.height - currentFrame.height
+    )
+  }
+  
+  private let coordinateSpaceName = UUID()
+  
+  public var body: some View {
     ScrollView(axes, showsIndicators: showsIndicators) {
       content()
-        .frameChanged { rect in
+        .frameChanged(coordinateSpace: .named(coordinateSpaceName)) { rect in
           currentContentFrame = rect
+          scrollAction?(offset, maxOffset)
         }
     }
-    .padding(.top, 1) // this will prevent the scrollview to extend under the safe area
+    .coordinateSpace(name: coordinateSpaceName)
+//    .padding([.top, .bottom], 1) // this will prevent the scrollview to extend under the safe area
+    .clipped() // this will prevent the scrollview to extend under the safe area
     .frame(minHeight: 0, maxHeight: currentContentFrame.height)
     .scrollDisabled(currentContentFrame.height <= currentFrame.height )
-    .frameChanged { rect in
+    .frameChanged(coordinateSpace: .named(coordinateSpaceName)) { rect in
       currentFrame = rect
     }
+  }
+}
+
+@available(iOS 16.0, *)
+extension SwaggerScrollView {
+  public func onScroll(perform action: @escaping ((_ offset: CGPoint, _ maxOffset: CGPoint) -> Void)) -> Self {
+    var copy = self
+    copy.scrollAction = action
+    return copy
   }
 }
 
@@ -37,12 +68,17 @@ struct SwaggerScrollView<Content: View>: View {
 struct SwaggerScrollView_Previews: PreviewProvider {
   static var previews: some View {
     SwaggerScrollView {
-      Text("Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla")
+      VeganIpsumView.long
+    }
+    .onScroll { offset, maxOffset in
+      print("⚙️ scroll offset: \(offset)")
+      print("🔚 max scroll offset: \(maxOffset)")
     }
     
     VStack(spacing: 0) {
       SwaggerScrollView {
-        Text("Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla")
+        VeganIpsumView.long
+          .padding([.leading, .trailing])
       }
       Spacer(minLength: 700)
     }
